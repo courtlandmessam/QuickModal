9fc035e840dff271666cbc3da458b933b629821c
courtlandmessam <messamcourtland@gmail.com>
2020-01-14 15:46:29 -0500

    upated open and close functionality of the modal

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const openModalButtons = document.querySelectorAll([data-modal-target]);
-const closeModalButtons = document.querySelectorAll([data-close-button]);
+const openModalButtons = document.querySelectorAll('[data-modal-target]');
+const closeModalButtons = document.querySelectorAll('[data-close-button]');
 const overlay = document.getElementById('overlay');
 
 openModalButtons.forEach(button => {
@@ -8,3 +8,29 @@ openModalButtons.forEach(button => {
     openModal(modal);
   })
 })
+
+overlay.addEventListener('click', () => {
+  const openModals = document.querySelectorAll('.modal.active');
+  openModals.forEach(modal => {
+    closeModal(modal);
+  })
+})
+
+closeModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = button.closest('.modal');
+    closeModal(modal);
+  })
+})
+
+function openModal(modal) {
+  if (modal == null) return;
+  modal.classList.add('active');
+  overlay.classList.add('active');
+}
+
+function closeModal(modal) {
+  if (modal == null) return;
+  modal.classList.remove('active');
+  overlay.classList.remove('active');
+}
